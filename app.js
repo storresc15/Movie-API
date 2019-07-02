e62894eca5ca0fe1d0c73328abd8ab3895a1cb14
Sebastian Torres <sebas.torres.88@gmail.com>
2019-07-02 00:08:43 +0000

    Changes applied to original app for deployment

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
 var request = require("request");
+
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", function(req, res) {
